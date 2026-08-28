365d5ebf079d1527b6d639636fcb746ddae3daef
vyshnavigandla3107-ui <vyshnavigandla3107@gmail.com>
2026-08-28 13:28:14 +0530

    Create demoProfile.js

---
+++ client /demoProfile.js
@@ -0,0 +1,10 @@
+export const demoStudentProfile = {
+  interests: ["Coding", "Artificial Intelligence", "Entrepreneurship"],
+  skills: ["Python programming", "Problem solving"],
+  preferredActivities: ["Workshops", "Competitions", "Hackathons"],
+  availableDays: ["Wednesday", "Friday"],
+  timeRangeStart: "16:00",
+  timeRangeEnd: "19:00",
+  preferredClubType: "Technical",
+  priority: "High"
+};
